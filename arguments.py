1a4b884b020a137176da98c6974ecb4063e30066
msyb <musuyaba@gmail.com>
2024-07-22 22:13:59 +0700

    docs: add docstrings on get_year function

--- arguments.py
+++ arguments.py
@@ -2,6 +2,13 @@ import argparse
 from datetime import datetime
 
 def get_year():
+    """
+    Fetches the year for which to retrieve holiday data. If no year is provided as an argument,
+    it defaults to the current year.
+
+    Returns:
+        int: The year for which to fetch the holiday data.
+    """
     parser = argparse.ArgumentParser(description='Fetch holiday data for a given year.')
     parser.add_argument('--year', type=int, help='The year for which to fetch the holiday data.')
     args = parser.parse_args()

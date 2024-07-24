910798005b136deef77d2a6133802fb789a30f13
msyb <musuyaba@gmail.com>
2024-07-24 13:44:59 +0700

    feat: save {year}.json and change logging to logger

--- main.py
+++ main.py
@@ -19,41 +19,49 @@ def main():
     storage_directory = os.getenv('STORAGE_DIRECTORY', 'storages')
     base_url = os.getenv('BASE_URL', 'https://tanggalan.com')
 
-    logging = setup_logging(log_directory)
-    logging.info("Script started")
+    logger = setup_logging(log_directory)
+    logger.info("Script started")
 
     year = get_year()
 
     year_directory = f'{storage_directory}/{year}'
     os.makedirs(year_directory, exist_ok=True)
 
+    year_file_path = f'{storage_directory}/{year}.json'
+    if os.path.exists(year_file_path):
+        logger.info(f'File for {year} already exists. Skipping.')
+        return
+
+    year_holidays = []
+
     url = f'{base_url}/{year}'
 
     try:
         response = requests.get(url)
         response.raise_for_status()
-        logging.info(f"Request to {url} successful")
+        logger.info(f"Request to {url} successful")
     except requests.RequestException as e:
-        logging.error(f"Request failed: {e}")
+        logger.error(f"Request failed: {e}")
         return
 
     try:
         soup = BeautifulSoup(response.content, 'html.parser')
-        logging.info("HTML content parsed successfully")
+        logger.info("HTML content parsed successfully")
     except Exception as e:
-        logging.error(f"Failed to parse HTML: {e}")
+        logger.error(f"Failed to parse HTML: {e}")
         return
 
     try:
         uls = soup.find_all('ul')
     except Exception as e:
-        logging.error(f"Failed to find <ul> elements: {e}")
+        logger.error(f"Failed to find <ul> elements: {e}")
         return
 
     for ul in uls:
         try:
             first_li = ul.find_all('li')[0]
             month_year = first_li.find('a').get_text().strip()
+            logger.debug(f"Processing month_year: {month_year}")
 
             match = re.match(r'([a-zA-Z]+)(\d+)', month_year)
             if match:
@@ -62,27 +70,28 @@ def main():
 
                 month = month_map.get(month_name)
                 if month is None:
-                    logging.warning(f"Month '{month_name}' not found in month_map")
+                    logger.warning(f"Month '{month_name}' not found in month_map")
                     continue
 
                 month_file_path = f'{year_directory}/{month}.json'
+                
                 if os.path.exists(month_file_path):
-                    logging.info(f'File for {year}-{month} already exists. Skipping.')
+                    logger.info(f'File for {year}-{month} already exists. Skipping.')
                     continue
-
+                
                 holidays = []
 
                 last_li = ul.find_all('li')[-1]
                 tables = last_li.find_all('table')
         except Exception as e:
-            logging.error(f"Failed to process <ul> element: {e}")
+            logger.error(f"Failed to process <ul> element: {e}")
             continue
 
         for table in tables:
             try:
                 trs = table.find_all('tr')
             except Exception as e:
-                logging.error(f"Failed to find <tr> elements: {e}")
+                logger.error(f"Failed to find <tr> elements: {e}")
                 continue
 
             for tr in trs:
@@ -91,6 +100,7 @@ def main():
                     if len(tds) >= 2:
                         date = tds[0].get_text().strip()
                         holiday_name = tds[1].get_text().strip()
+                        logger.debug(f"Found holiday: {holiday_name} on date(s): {date}")
 
                         if '-' in date:
                             start_day, end_day = map(int, date.split('-'))
@@ -102,6 +112,12 @@ def main():
                                     'holiday': holiday_name,
                                     'collective_leave': collective_leave
                                 })
+                                year_holidays.append({
+                                    'date': formatted_date,
+                                    'holiday': holiday_name,
+                                    'collective_leave': collective_leave
+                                })
+                                logger.debug(f"Appended holiday: {holiday_name} on {formatted_date}")
                         else:
                             formatted_date = f'{extracted_year}-{month}-{date.zfill(2)}'
                             collective_leave = 'cuti' in holiday_name.lower()
@@ -110,18 +126,32 @@ def main():
                                 'holiday': holiday_name,
                                 'collective_leave': collective_leave
                             })
+                            year_holidays.append({
+                                'date': formatted_date,
+                                'holiday': holiday_name,
+                                'collective_leave': collective_leave
+                            })
+                            logger.debug(f"Appended holiday: {holiday_name} on {formatted_date}")
                 except Exception as e:
-                    logging.error(f"Failed to process <tr> element: {e}")
+                    logger.error(f"Failed to process <tr> element: {e}")
                     continue
 
         try:
             with open(month_file_path, 'w', encoding='utf-8') as json_file:
                 json.dump(holidays, json_file, ensure_ascii=False, indent=4)
-            logging.info(f'Holidays data for {year}-{month} saved to {month_file_path}')
+            logger.info(f'Holidays data for {year}-{month} saved to {month_file_path}')
         except Exception as e:
-            logging.error(f"Failed to write JSON file: {e}")
+            logger.error(f"Failed to write JSON file: {e}")
 
-    logging.info("Script finished")
+    try:
+        logger.debug(f"Year holidays: {year_holidays}")
+        with open(year_file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(year_holidays, json_file, ensure_ascii=False, indent=4)
+            logger.info(f'Holidays data for {year} saved to {year_file_path}')
+    except Exception as e:
+        logger.error(f"Failed to write JSON file: {e}")
+
+    logger.info("Script finished")
 
 if __name__ == "__main__":
     main()
